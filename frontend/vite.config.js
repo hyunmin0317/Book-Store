ecb661cfdfccbc681bd868b89e3127dd910e48d1
hyunmin0317 <choihm9903@naver.com>
2023-11-26 16:29:03 +0900

    chore: vite 연동 설정 (#2)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    outDir: "../backend/src/main/resources/static",
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:8080",
+    },
+  },
+});
+
