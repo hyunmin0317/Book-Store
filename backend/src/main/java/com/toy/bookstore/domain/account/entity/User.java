2709247a07797b0a61f5c0f7fe478f4419260975
hyunmin0317 <choihm9903@naver.com>
2023-11-26 22:32:17 +0900

    feat: User 엔티티 수정 (#13)

--- backend/src/main/java/com/toy/bookstore/domain/account/entity/User.java
+++ backend/src/main/java/com/toy/bookstore/domain/account/entity/User.java
@@ -1,12 +1,18 @@
 package com.toy.bookstore.domain.account.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
