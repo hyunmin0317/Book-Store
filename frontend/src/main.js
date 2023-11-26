a5fbc728a3f44888883548a27f88cf760234307d
hyunmin0317 <choihm9903@naver.com>
2023-11-26 19:31:35 +0900

    feat: frontend 프로젝트 (#2)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router';
+import './common/plugins/bootstrap-vue'
+import './common/plugins/vue-slick'
+Vue.config.productionTip = false
 
-createApp(App).mount('#app')
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
