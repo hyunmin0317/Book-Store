3c26ce8572769242a9fef27e81d16948daaeffc2
hyunmin0317 <choihm9903@naver.com>
2023-11-28 20:31:20 +0900

    feat: UserDto 수정 (#12)

--- backend/src/main/java/com/toy/bookstore/domain/account/dto/UserDto.java
+++ backend/src/main/java/com/toy/bookstore/domain/account/dto/UserDto.java
@@ -1,26 +1,21 @@
 package com.toy.bookstore.domain.account.dto;
 
 import com.toy.bookstore.domain.account.entity.User;
-import com.toy.bookstore.global.common.CommonResponse;
 import lombok.Builder;
 import lombok.Data;
+
 
 public class UserDto {
     @Data
     @Builder
     public static class Response {
-        private boolean success;
-        private int code;
-        private String msg;
+        private String username;
         private String token;
 
-        public static Response of(boolean success, String token) {
-            CommonResponse response = success ? CommonResponse.SUCCESS : CommonResponse.FAIL;
+        public static Response of(String username, String token) {
             return Response.builder()
-                    .success(success)
+                    .username(username)
                     .token(token)
-                    .code(response.getCode())
-                    .msg(response.getMsg())
                     .build();
         }
     }
